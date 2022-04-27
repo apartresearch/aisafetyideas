ce846f521e15635ed9a22c7b4c5a1e25ea71a3b9
dr-kranc <esben@eachthing.com>
2022-04-27 03:46:15 -0700

    vercel

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  meta.process.env.VITE_SUPABASE_URL,
-  meta.process.env.VITE_SUPABASE_ANON_KEY
+  process.env.VITE_SUPABASE_URL,
+  process.env.VITE_SUPABASE_ANON_KEY
 )
 
 export default supabase
