6c15b8f4286771c3a5ebfce006fcb5af4753fdf4
dr-kranc <esben@eachthing.com>
2022-04-27 03:52:53 -0700

    vercel

--- src/lib/db.js
+++ src/lib/db.js
@@ -1,8 +1,10 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabase = createClient(
-  process.env.VITE_SUPABASE_URL,
-  process.env.VITE_SUPABASE_ANON_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+
+
 )
 
 export default supabase
--- svelte.config.js
+++ svelte.config.js
@@ -4,6 +4,11 @@ import adapter from '@sveltejs/adapter-auto';
 const config = {
 	kit: {
 		adapter: adapter(),
+		vite: {
+			define: {
+				'process.env': process.env,
+			},
+		},
 
 		// hydrate the <div id="svelte"> element in src/app.html
 		target: '#svelte'
