9ddaa050cb266398c16d0a4591860848cae2a0a3
dr-kranc <esben@eachthing.com>
2022-05-16 17:37:41 -0400

    Updated front page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -3,6 +3,11 @@
   import { onMount } from "svelte";
   import Select from "svelte-select";
 
+  let ideas = [],
+    superprojects = [],
+    categories = [],
+    problems = [];
+
   onMount(async () => {
     ideas = await getTable("ideas");
     superprojects = await getTable("idea_superprojects");
@@ -11,6 +16,34 @@
   });
 </script>
 
+<header id="nav" class="sticky-nav">
+  <nav class="container w-container">
+    <ul role="list" class="nav-grid w-list-unstyled">
+      <li class="list-item">
+        <a href="/" aria-current="page" class="nav-logo-link w--current">
+          <img
+            src="https://uploads-ssl.webflow.com/622160bba1d5c0dcf96f8bdf/62431292c50af756943fd210_ideas_icon.png"
+            alt="AI safety ideas logo"
+            class="nav-logo"
+          />
+        </a>
+        <h1 class="logotext">AI Safety Research Ideas</h1>
+      </li>
+      <li id="w-node-_8db6f7ee-7804-6955-cbb7-65390522c9f3-81dce09d">
+        <div class="html-embed w-embed">
+          <button
+            data-tally-open="mZqqAn"
+            data-tally-hide-title="1"
+            class="button utility w-button"
+          >
+            Submit an idea
+          </button>
+        </div>
+      </li>
+      <li><a href="#" class="button utility w-button"> Dark mode </a></li>
+    </ul>
+  </nav>
+</header>
 <h2>See ideas</h2>
 {#each ideas as idea}
   <i>{idea.author}</i><br />
@@ -21,4 +54,7 @@
 {/each}
 
 <style>
+  h2 {
+    text-align: center;
+  }
 </style>
