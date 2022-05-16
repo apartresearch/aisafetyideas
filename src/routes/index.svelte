c1676298a81cd159534387ee6fb62d43ba0346d3
dr-kranc <esben@eachthing.com>
2022-05-16 17:30:21 -0400

    Updated front page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,95 +1,24 @@
 <script>
-  import { goto } from "$app/navigation";
-
   import supabase from "$lib/db";
-  import { user } from "$lib/stores";
-  import Todo from "$lib/Idea.svelte";
   import { onMount } from "svelte";
-
-  let ideas = [];
-  let superprojects = [];
-  let categories = [];
-  let ideaTitle = "";
-  let session = null;
+  import Select from "svelte-select";
 
   onMount(async () => {
-    await getAllIdeas();
-    await getAllSuperprojects();
-    await getAllCategories();
+    ideas = await getTable("ideas");
+    superprojects = await getTable("idea_superprojects");
+    categories = await getTable("idea_categories");
+    problems = await getTable("problems");
   });
-
-  const getAllIdeas = async () => {
-    try {
-      let { data, error } = await supabase.from("ideas").select("*");
-      ideas = data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const addNewIdea = async (title) => {
-    try {
-      const { data, error } = await supabase
-        .from("ideas")
-        .insert([{ task: newTask, user_id: "22" }]);
-      await getAllIdeas();
-      newTask = "";
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const updateIdea = async (idea) => {
-    try {
-      const { data, error } = await supabase
-        .from("ideas")
-        .update({ task: idea.task, isComplete: idea.isComplete })
-        .eq("id", idea.id);
-      await getAllIdeas();
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getAllSuperprojects = async () => {
-    try {
-      let { data, error } = await supabase
-        .from("idea_superprojects")
-        .select("*");
-      superprojects = data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const getAllCategories = async () => {
-    try {
-      let { data, error } = await supabase.from("idea_categories").select("*");
-      superprojects = data;
-    } catch (err) {
-      console.log(err);
-    }
-  };
-
-  const handleKeyPress = (event) => {
-    if (event.key === "Enter" && newTask !== "") {
-      addNewTodo();
-    }
-  };
 </script>
 
-<div class="add-todo">
-  <input type="text" bind:value={ideaTitle} />
-  <button on:click={() => addNewIdea()}>Submit idea</button>
-</div>
-
+<h2>See ideas</h2>
 {#each ideas as idea}
-  <p>IDEA HERE WOW! {JSON.stringify(idea)}</p>
+  <i>{idea.author}</i><br />
+  <b>{idea.title}</b><br />
+  {@html markdown(idea.summary)}
 {:else}
   <p>No ideas found</p>
 {/each}
-
-<svelte:window on:keypress={handleKeyPress} />
 
 <style>
 </style>
